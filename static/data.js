89b24d07cf2292ead21557455f26d77d6e5e6de6
Samin <samincgs@gmail.com>
2025-08-19 20:29:57 -0400

    added more structure to the code but making a different js file to hold the data

---
+++ static/data.js
@@ -0,0 +1,81 @@
+selfQuotes = [
+  'Software developer with over 4 years of experience building software solutions. My expertise includes Python, JavaScript, and more.',
+  'Experienced software developer with 4+ years in creating software solutions. Skilled in Python, JavaScript, and other technologies.',
+  'I am a software developer with over 4 years of experience, specializing in Python, JavaScript, and building efficient software solutions.',
+]
+
+workDetails = {
+  osc: {
+    name: 'Ontario Science Centre',
+    role: 'Data Analyst',
+    description: '',
+    tools: [
+      'Power Platform',
+      'Excel',
+      'PowerBI',
+      'SQL',
+      'Python',
+      'HTML',
+      'CSS',
+    ],
+  },
+  tbs: {
+    name: 'Treasury Board Secretiat',
+    role: 'Business Finance Analyst',
+    description: '',
+    tools: ['Excel', 'Planview'],
+  },
+  wi: {
+    name: 'WiConnect',
+    role: 'Junior IT Support',
+    description: '',
+    tools: ['Excel'],
+  },
+}
+
+personalProjects = {
+  'Persona Notepad': {
+    description:
+      'A notepad that mixes my favorite coding language with my favorite game',
+    tools: ['Python', 'Tkinter', 'Pillow', 'PyAudio'],
+    image: 'images/persona-notepad.png',
+    url: 'https://github.com/samincgs/persona-notepad',
+  },
+  '2D Adventure Game': {
+    description: 'An Old School Pokemon Style RPG game.',
+    tools: ['Python', 'Pygame', 'JSON'],
+    image: 'images/2dadventuregame.png',
+    url: 'https://github.com/samincgs/2DAdventure',
+  },
+
+  'Chess Engine': {
+    description: 'A Chess Engine',
+    tools: ['Python', 'Pygame'],
+    image: 'images/chess.png',
+    url: 'https://github.com/samincgs/chess',
+  },
+}
+
+workProjects = {
+  'Netflix Show Release Trend Analysis': {
+    description:
+      'A notepad that mixes my favorite coding language with my favorite game',
+    tools: ['Excel', 'SQL', 'Python', 'Pandas', 'Matplotlib', 'Scikit-Learn'],
+    image: 'images/netflix_trend.png',
+    url: 'https://github.com/samincgs/netflix-show-release-trend-analysis',
+  },
+  'Loan Repayment Factors Analysis': {
+    description:
+      'A notepad that mixes my favorite coding language with my favorite game',
+    tools: ['Excel', 'Python', 'Pandas', 'NumPy', 'Matplotlib', 'Scipy'],
+    image: 'images/loan_repayment.png',
+    url: 'https://github.com/samincgs/netflix-show-release-trend-analysis',
+  },
+  Quill: {
+    description:
+      'A notepad that mixes my favorite coding language with my favorite game',
+    tools: ['Python', 'Flask', 'MongoDB', 'HTML', 'CSS', 'JavaScript'],
+    image: 'images/quill.png',
+    url: 'https://github.com/samincgs/quill',
+  },
+}
--- static/index.js
+++ static/index.js
@@ -1,98 +1,13 @@
 const navbar = document.querySelector('.navbar')
 const buttons = document.querySelectorAll('.navbar-items button')
 const sections = document.querySelectorAll('section')
-const project_cards = document.querySelector('.project-cards')
+const projectsCard = document.querySelector('.project-cards')
 
 let projectChoice = 'professional'
 
-const quotes = [
-  'Software developer with over 4 years of experience building software solutions. My expertise includes Python, JavaScript, and more.',
-  'Experienced software developer with 4+ years in creating software solutions. Skilled in Python, JavaScript, and other technologies.',
-  'I am a software developer with over 4 years of experience, specializing in Python, JavaScript, and building efficient software solutions.',
-]
-
-const workDetails = {
-  osc: {
-    name: 'Ontario Science Centre',
-    role: 'Data Analyst',
-    work_period: ['Jan 2025', 'Dec 2025'],
-    description: '',
-    tools: [
-      'Power Platform',
-      'Excel',
-      'PowerBI',
-      'SQL',
-      'Python',
-      'HTML',
-      'CSS',
-    ],
-  },
-  tbs: {
-    name: 'Treasury Board Secretiat',
-    role: 'Business Finance Analyst',
-    work_period: ['Jan 2024', 'Apr 2024'],
-    description: '',
-    tools: ['Excel', 'Planview'],
-  },
-  wi: {
-    name: 'WiConnect',
-    role: 'Junior IT Support',
-    work_period: ['Jan 2020', 'May 2020'],
-    description: '',
-    tools: ['Excel'],
-  },
-}
-
-const personal_projects = {
-  'Persona Notepad': {
-    description:
-      'A notepad that mixes my favorite coding language with my favorite game',
-    tools: ['Python', 'Tkinter', 'Pillow', 'PyAudio'],
-    image: 'images/persona-notepad.png',
-    url: 'https://github.com/samincgs/persona-notepad',
-  },
-  '2D Adventure Game': {
-    description: 'An Old School Pokemon Style RPG game.',
-    tools: ['Python', 'Pygame', 'JSON'],
-    image: 'images/2dadventuregame.png',
-    url: 'https://github.com/samincgs/2DAdventure',
-  },
-
-  'Chess Engine': {
-    description: 'A Chess Engine',
-    tools: ['Python', 'Pygame'],
-    image: 'images/chess.png',
-    url: 'https://github.com/samincgs/chess',
-  },
-}
-
-const work_projects = {
-  'Netflix Show Release Trend Analysis': {
-    description:
-      'A notepad that mixes my favorite coding language with my favorite game',
-    tools: ['Excel', 'SQL', 'Python', 'Pandas', 'Matplotlib', 'Scikit-Learn'],
-    image: 'images/netflix_trend.png',
-    url: 'https://github.com/samincgs/netflix-show-release-trend-analysis',
-  },
-  'Loan Repayment Factors Analysis': {
-    description:
-      'A notepad that mixes my favorite coding language with my favorite game',
-    tools: ['Excel', 'Python', 'Pandas', 'NumPy', 'Matplotlib', 'Scipy'],
-    image: 'images/loan_repayment.png',
-    url: 'https://github.com/samincgs/netflix-show-release-trend-analysis',
-  },
-  Quill: {
-    description:
-      'A notepad that mixes my favorite coding language with my favorite game',
-    tools: ['Python', 'Flask', 'MongoDB', 'HTML', 'CSS', 'JavaScript'],
-    image: 'images/quill.png',
-    url: 'https://github.com/samincgs/quill',
-  },
-}
-
-projects_set = {
-  personal: personal_projects,
-  professional: work_projects,
+const projectsSet = {
+  personal: personalProjects,
+  professional: workProjects,
 }
 
 function smoothToSection(sectionId) {
@@ -143,33 +58,34 @@ function updateNavbar() {
 }
 
 function changingText() {
-  span_text = document.querySelector('.changing-text')
+  spanText = document.querySelector('.changing-text')
 
   let index = 0
 
   function showNextQuote() {
-    span_text.style.opacity = 0
+    spanText.style.opacity = 0
 
     setTimeout(() => {
-      span_text.innerHTML = quotes[index]
-      span_text.style.opacity = 1
-      index = (index + 1) % quotes.length
+      spanText.innerHTML = selfQuotes[index]
+      spanText.style.opacity = 1
+      index = (index + 1) % selfQuotes.length
     }, 600)
   }
 
-  span_text.innerHTML = quotes[index]
-  span_text.style.opacity = 1
+  spanText.innerHTML = selfQuotes[index]
+  spanText.style.opacity = 1
   index++
 
   setInterval(showNextQuote, 7000)
 }
 
 function addProjects(projects) {
-  project_cards.innerHTML = ''
+  projectsCard.innerHTML = ''
 
   Object.entries(projects).forEach(([projectName, projectData]) => {
     const a = document.createElement('a')
     a.href = projectData.url
+
     const projectCard = document.createElement('div')
     projectCard.classList.add('project-card')
 
@@ -203,20 +119,20 @@ function addProjects(projects) {
 
     gameInfo.appendChild(toolDiv)
     a.appendChild(projectCard)
-    project_cards.appendChild(a)
+    projectsCard.appendChild(a)
   })
 }
 
 function updateProjects(choice) {
   if (projectChoice != choice) {
     projectChoice = choice
-    addProjects(projects_set[projectChoice])
+    addProjects(projectsSet[projectChoice])
   }
 }
 
 // initial calls
 changingText()
-addProjects(projects_set[projectChoice])
+addProjects(projectsSet[projectChoice])
 updateActiveButtons()
 updateNavbar()
 
